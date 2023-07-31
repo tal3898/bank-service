51b1be1fece2bd0ccf8584a760d4b5a908927593
tal <tal.bny3@gmail.com>
2023-07-31 14:54:00 +0300

    added a connection to db

---
+++ src/dal/connection.ts
@@ -0,0 +1,27 @@
+import { Db, MongoClient } from 'mongodb';
+
+export class Connection {
+  // Connection URL
+   private url: string;
+  
+  // Database Name
+   private dbName: string;
+   private client: MongoClient
+   private db: Db | undefined;
+
+   constructor() {
+    this.url = 'mongodb://localhost:27017';
+    this.dbName = 'bank';
+    this.client = new MongoClient(this.url);
+   }
+
+   async connect() {
+    try {
+      await this.client.connect();
+      this.db = this.client.db(this.dbName);
+    } catch (err) {
+      console.error(err);
+    }
+  }
+
+}
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,8 @@
+import {Connection} from './dal/connection';
+
+
+const dbConnection = new Connection();
+dbConnection.connect().then((con) => {
+    console.log('connected')
+});
 console.log('Hello, TypeScript!');
